6e12288ba7f130a9ec914b7fec4f647b7213df6e
tsomani <tabreek.somani@gmail.com>
2023-03-14 08:24:42 -0700

    Public Photo Gallery and Photo Upload

--- server/api/controllers/photoController.js
+++ server/api/controllers/photoController.js
@@ -1,6 +1,7 @@
 import User from '../models/user.js'
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import fs from "fs";
 
 dotenv.config();
 const secretKey = process.env.SECRET_KEY;
@@ -36,5 +37,62 @@ export default class PhotoController {
 
         });
     }
+
+    addPhoto(req) {
+        return new Promise(async (resolve, reject) => {
+            let buffer
+            if (req.file == null) {
+                // If Submit was accidentally clicked with no file selected...
+                return reject("No file uploaded")
+            } else {
+                // read the img file from tmp in-memory location
+                const newImg = fs.readFileSync(req.file.path);
+                // encode the file as a base64 string.
+                const encImg = newImg.toString('base64');
+                // define your new document
+                buffer = new Buffer(encImg, 'base64')
+            }
+            const userId = this.verifyToken(req);
+            if (userId === null) return reject("Unauthorized Request");
+            const newPhoto = {
+                created: Date.now(),
+                img: {
+                    data: buffer,
+                    contentType: 'image/jpeg'
+                }
+            };
+
+            return User.findByIdAndUpdate(userId, {$push: {photos: newPhoto}})
+                .then(() => {
+                    return resolve('Photo added successfully');
+                })
+                .catch(err => {
+                    return reject('Error adding photo ' + err);
+                });
+
+        });
+    }
+
+    getPublicPhotos(req) {
+        return new Promise(async (resolve, reject) => {
+            if (this.verifyToken(req) === null) return reject("Unauthorized Request");
+            const users = await User.find();
+            let gallery = [];
+            for (let user of users) {
+                if (user.isPublic) {
+                    user.photos.map((photo) => {
+                        gallery.push(photo);
+                    })
+                }
+            }
+
+            gallery = gallery.sort(function (a, b) {
+                return b.created - a.created;
+            });
+            return resolve(gallery);
+
+        });
+    }
 }
+
 
--- server/api/routes/photoRoute.js
+++ server/api/routes/photoRoute.js
@@ -1,8 +1,19 @@
 import {Router} from "express";
 import PhotoController from "../controllers/photoController.js";
+import multer from "multer";
 
 const router = Router();
 const photoController = new PhotoController();
+import mime from 'mime-types';
+
+const upload = multer({
+    dest: 'uploads/',
+    filename: function (req, file, cb) {
+        const extension = mime.extension(file.mimetype);
+        cb(null, req.file.originalname + extension);
+    }
+})
+
 
 router.delete('/:photoId', (req, res) => {
     if (!req) {
@@ -20,8 +31,36 @@ router.delete('/:photoId', (req, res) => {
         });
 });
 
+router.post('/', upload.single('image'), (req, res) => {
+    if (!req) {
+        res.status(400).send({
+            message: "Content can not be empty!",
+        });
+        return;
+    }
+    return photoController.addPhoto(req)
+        .then((response) => {
+            return res.status(200).send(response);
+        })
+        .catch((err) => {
+            return res.status(404).json(err);
+        });
+});
+
 router.get('/', (req, res) => {
-    res.send('Hello, world!');
+    if (!req) {
+        res.status(400).send({
+            message: "Content can not be empty!",
+        });
+        return;
+    }
+    return photoController.getPublicPhotos(req)
+        .then((response) => {
+            return res.status(200).send(response);
+        })
+        .catch((err) => {
+            return res.status(404).json(err);
+        });
 });
 
 export default router;
